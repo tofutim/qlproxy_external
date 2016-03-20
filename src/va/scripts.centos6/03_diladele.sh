5fd7aafe452fcb298176f16b2ae7fed8b45bb8bb
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-03-20 16:08:54 +0100

    scripts to build centos 6 based virtual appliance

---
+++ src/va/scripts.centos6/03_diladele.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# all packages are installed as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# default arc
+MAJOR="4.4.0"
+MINOR="E337"
+
+# get latest qlproxy
+curl -O http://packages.diladele.com/qlproxy/$MAJOR.$MINOR/amd64/release/centos6/qlproxy-${MAJOR}-${MINOR}.x86_64.rpm
+
+# install it
+yum -y --nogpgcheck localinstall qlproxy-${MAJOR}-${MINOR}.x86_64.rpm
+  
+# qlproxy installed everything needed for apache, so just restart
+service httpd start
