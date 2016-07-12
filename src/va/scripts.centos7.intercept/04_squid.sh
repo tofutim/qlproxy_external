db0762b675e2439ce93172c67a30bd9c4e234016
Rafael Akchurin <rafael.akchurin@diladele.com>
2016-07-12 11:08:01 +0200

    the squid is now 3.5.20 in both normal and transparent scripts

--- src/va/scripts.centos7.intercept/04_squid.sh
+++ src/va/scripts.centos7.intercept/04_squid.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# stop on every error
-set -e
-
 # install RPMs as root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-# install stock squid
-yum -y install squid
+# enable epel repository
+yum -y install epel-release && yum -y update
+
+# install required perl module
+yum -y install perl-Crypt-OpenSSL-X509
+
+# get latest squid and ecap library
+curl -O http://www1.ngtech.co.il/repo/centos/7/x86_64/libecap-1.0.0-3.el7.centos.x86_64.rpm
+curl -O http://www1.ngtech.co.il/repo/centos/7/x86_64/squid-3.5.20-1.el7.centos.x86_64.rpm
+curl -O http://www1.ngtech.co.il/repo/centos/7/x86_64/squid-helpers-3.5.20-1.el7.centos.x86_64.rpm
+
+# and install it
+yum -y --nogpgcheck localinstall libecap-1.0.0-3.el7.centos.x86_64.rpm
+yum -y --nogpgcheck localinstall squid-3.5.20-1.el7.centos.x86_64.rpm
+yum -y --nogpgcheck localinstall squid-helpers-3.5.20-1.el7.centos.x86_64.rpm
 
 # make squid autostart after reboot
 systemctl enable squid.service
